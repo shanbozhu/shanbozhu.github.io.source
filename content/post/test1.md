8417c221e14351a8306d1e0a27c15f532f79e856
Zhu,Shanbo <zhushanbo@baidu.com>
2019-05-19 22:32:38 +0800

    no message

--- content/post/test1.md
+++
@@ -1,6 +0,0 @@
----
-title: "Test1"
-date: 2019-05-19T22:07:34+08:00
-draft: false
----
-
--- update.sh
+++ update.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-echo "开始执行hugo"
-hugo
-echo "完成执行hugo"
-echo "开始执行拷贝"
 cp -a public/* ~/Desktop/shanbozhu.github.io
-echo "完成执行拷贝"
