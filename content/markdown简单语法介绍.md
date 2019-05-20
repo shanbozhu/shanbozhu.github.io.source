e2e7618199f06da1eaf2cfd9d9bb1b16d69a9b1c
Zhu,Shanbo <zhushanbo@baidu.com>
2019-05-20 14:17:19 +0800

    no message

---
+++ content/markdown简单语法介绍.md
@@ -0,0 +1,6 @@
+---
+title: "Markdown简单语法介绍"
+date: 2019-05-20T14:16:17+08:00
+draft: false
+---
+
--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 cp -a public/* ~/Desktop/shanbozhu.github.io
+cd ~/Desktop/shanbozhu.github.io
+git add .
+git commit -m "no message"
+git push
